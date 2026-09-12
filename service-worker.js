bea89c4fb92e1cbff0f93a5eaa7ea36d0c1098ee
msadk2 <msadk2@yahoo.es>
2026-09-12 02:04:20 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "spinningtv-pwa-v10";
+const CACHE_NAME = "spinningtv-pwa-v11";
 const APP_SHELL = [
   "./cartelera.html",
-  "./cartelera.css?v=20260912-tvplus1",
-  "./cartelera-common.js?v=20260912-tvplus1",
+  "./cartelera.css?v=20260912-tvplus2",
+  "./cartelera-common.js?v=20260912-tvplus2",
   "./spinning-ibo-logo-transparent.png",
   "./app-icon.png",
   "./manifest-cartelera.webmanifest"
